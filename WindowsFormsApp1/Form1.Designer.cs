69b6d1b2153e5144990b4160b786a894be7c31c0
valer <valer@MARTINBRONI>
2024-06-17 13:34:06 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Timers;
+using System.Diagnostics;
+
+namespace PlaytoztheGame
+{
+    public partial class MainForm : Form
+    {
+        private bool[,] gameGridPlayer1; // Игровое поле для игрока 1
+        private bool[,] gameGridPlayer2; // Игровое поле для игрока 2
+        private int width = 50; // Ширина игрового поля
+        private int height = 50; // Высота игрового поля
+        private int cellSize = 10; // Размер клетки
+        private Color player1Color = Color.Blue; // Цвет клеток первого игрока
+        private Color player2Color = Color.Red; // Цвет клеток второго игрока
+        private Color backgroundColor = Color.White; // Цвет фона
+        private bool isPlayer1Turn = true; // Переменная для определения очередности игроков
+        private System.Timers.Timer gameTimer;
+        private int generationInterval = 200; // Интервал между поколениями в миллисекундах
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            // Инициализация игровых полей для обоих игроков
+            gameGridPlayer1 = new bool[width, height];
+            gameGridPlayer2 = new bool[width, height];
+
+            // Инициализация таймера
+            gameTimer = new System.Timers.Timer(generationInterval);
+            gameTimer.Elapsed += GameTimer_Elapsed;
+
+            // Отображение начального состояния игры
+            UpdateGameUI();
+        }
+
+        private void UpdateGameUI()
+        {
+            // Перерисовка игрового поля
+            gamePanel.Invalidate();
+        }
+
+        private void GamePanel_Paint(object sender, PaintEventArgs e)
+        {
+            // Заливка фона
+            e.Graphics.Clear(backgroundColor);
+
+            // Отрисовка клеток для обоих игроков
+            DrawPlayerGrid(e.Graphics, gameGridPlayer1, player1Color); // Игрок 1
+            DrawPlayerGrid(e.Graphics, gameGridPlayer2, player2Color); // Игрок 2
+        }
+
+        private void DrawPlayerGrid(Graphics g, bool[,] gameGrid, Color color)
+        {
+            Brush brush = new SolidBrush(color);
+
+            // Отрисовка клеток для определённого игрока
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (gameGrid[i, j])
+                    {
+                        // Рисуем прямоугольник для отображения живой клетки
+                        g.FillRectangle(brush, i * cellSize, j * cellSize, cellSize, cellSize);
+                    }
+                }
+            }
+        }
+
+        private void GamePanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Получение координат клика
+            int xIndex = e.X / cellSize;
+            int yIndex = e.Y / cellSize;
+
+            // Проверка границ и обновление состояния клетки
+            if (xIndex >= 0 && xIndex < width && yIndex >= 0 && yIndex < height)
+            {
+                if (isPlayer1Turn)
+                {
+                    gameGridPlayer1[xIndex, yIndex] = !gameGridPlayer1[xIndex, yIndex]; // Инвертирование состояния клетки для игрока 1
+                }
+                else
+                {
+                    gameGridPlayer2[xIndex, yIndex] = !gameGridPlayer2[xIndex, yIndex]; // Инвертирование состояния клетки для игрока 2
+                }
+
+                // Переключение хода на следующего игрока
+                isPlayer1Turn = !isPlayer1Turn;
+
+                // Обновление отображения
+                UpdateGameUI();
+            }
+        }
+
+        private void Player1ColorButton_Click(object sender, EventArgs e)
+        {
+            // Диалог выбора цвета для игрока 1
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                player1Color = colorDialog.Color;
+                UpdateGameUI();
+            }
+        }
+
+        private void Player2ColorButton_Click(object sender, EventArgs e)
+        {
+            // Диалог выбора цвета для игрока 2
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                player2Color = colorDialog.Color;
+                UpdateGameUI();
+            }
+        }
+
+        private void BackgroundColorButton_Click(object sender, EventArgs e)
+        {
+            // Диалог выбора цвета фона
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                backgroundColor = colorDialog.Color;
+                UpdateGameUI();
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            // Очистка игрового поля обоих игроков
+            gameGridPlayer1 = new bool[width, height];
+            gameGridPlayer2 = new bool[width, height];
+
+            // Сброс переменной очерёдности игроков
+            isPlayer1Turn = true;
+
+            // Обновление отображения
+            UpdateGameUI();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            // Логика запуска игры
+            gameTimer.Start();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            // Логика остановки игры
+            gameTimer.Stop();
+        }
+
+        private void GameTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            // Логика обновления состояния игры для каждого поколения
+            bool[,] newGameGridPlayer1 = new bool[width, height];
+            bool[,] newGameGridPlayer2 = new bool[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    int aliveNeighborsPlayer1 = CountAliveNeighbors(gameGridPlayer1, i, j);
+                    int aliveNeighborsPlayer2 = CountAliveNeighbors(gameGridPlayer2, i, j);
+
+                    // Правила игры для игрока 1
+                    if (gameGridPlayer1[i, j])
+                    {
+                        if (aliveNeighborsPlayer1 < 2 || aliveNeighborsPlayer1 > 3)
+                        {
+                            newGameGridPlayer1[i, j] = false; // Клетка умирает
+                        }
+                        else
+                        {
+                            newGameGridPlayer1[i, j] = true; // Клетка остаётся живой
+                        }
+                    }
+                    else
+                    {
+                        if (aliveNeighborsPlayer1 == 3)
+                        {
+                            newGameGridPlayer1[i, j] = true; // Клетка оживает
+                        }
+                    }
+
+                    // Правила игры для игрока 2
+                    if (gameGridPlayer2[i, j])
+                    {
+                        if (aliveNeighborsPlayer2 < 2 || aliveNeighborsPlayer2 > 3)
+                        {
+                            newGameGridPlayer2[i, j] = false; // Клетка умирает
+                        }
+                        else
+                        {
+                            newGameGridPlayer2[i, j] = true; // Клетка остаётся живой
+                        }
+                    }
+                    else
+                    {
+                        if (aliveNeighborsPlayer2 == 3)
+                        {
+                            newGameGridPlayer2[i, j] = true; // Клетка оживает
+                        }
+                    }
+                }
+            }
+
+            // Обновление игровых полей
+            gameGridPlayer1 = newGameGridPlayer1;
+            gameGridPlayer2 = newGameGridPlayer2;
+
+            // Обновление отображения
+            UpdateGameUI();
+        }
+
+        private int CountAliveNeighbors(bool[,] gameGrid, int x, int y)
+        {
+            int aliveNeighbors = 0;
+
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0)
+                        continue;
+
+                    int nx = x + i;
+                    int ny = y + j;
+
+                    if (nx >= 0 && nx < width && ny >= 0 && ny < height && gameGrid[nx, ny])
+                    {
+                        aliveNeighbors++;
+                    }
+                }
+            }
+
+            return aliveNeighbors;
+        }
+
+
+        private void OpenSocialMediaLink(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось открыть ссылку: " + ex.Message);
+            }
+        }
+
+        private void TelegramButton_Click(object sender, EventArgs e)
+        {
+            OpenSocialMediaLink("https://t.me/filmbroni");
+        }
+
+
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PlaytoztheGame
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.player1ColorButton = new System.Windows.Forms.Button();
+            this.player2ColorButton = new System.Windows.Forms.Button();
+            this.backgroundColorButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.logoLabel = new System.Windows.Forms.Label();
+            this.telegramButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Location = new System.Drawing.Point(12, 12);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(500, 500);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.GamePanel_Paint);
+            this.gamePanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.GamePanel_MouseClick);
+            // 
+            // player1ColorButton
+            // 
+            this.player1ColorButton.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.player1ColorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.player1ColorButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.player1ColorButton.Location = new System.Drawing.Point(521, 50);
+            this.player1ColorButton.Name = "player1ColorButton";
+            this.player1ColorButton.Size = new System.Drawing.Size(120, 32);
+            this.player1ColorButton.TabIndex = 1;
+            this.player1ColorButton.Text = "Цвет игрока 1";
+            this.player1ColorButton.UseVisualStyleBackColor = false;
+            this.player1ColorButton.Click += new System.EventHandler(this.Player1ColorButton_Click);
+            // 
+            // player2ColorButton
+            // 
+            this.player2ColorButton.BackColor = System.Drawing.Color.Violet;
+            this.player2ColorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.player2ColorButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.player2ColorButton.Location = new System.Drawing.Point(521, 88);
+            this.player2ColorButton.Name = "player2ColorButton";
+            this.player2ColorButton.Size = new System.Drawing.Size(120, 32);
+            this.player2ColorButton.TabIndex = 2;
+            this.player2ColorButton.Text = "Цвет игрока 2";
+            this.player2ColorButton.UseVisualStyleBackColor = false;
+            this.player2ColorButton.Click += new System.EventHandler(this.Player2ColorButton_Click);
+            // 
+            // backgroundColorButton
+            // 
+            this.backgroundColorButton.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.backgroundColorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.backgroundColorButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.backgroundColorButton.Location = new System.Drawing.Point(521, 12);
+            this.backgroundColorButton.Name = "backgroundColorButton";
+            this.backgroundColorButton.Size = new System.Drawing.Size(120, 32);
+            this.backgroundColorButton.TabIndex = 3;
+            this.backgroundColorButton.Text = "Цвет фона";
+            this.backgroundColorButton.UseVisualStyleBackColor = false;
+            this.backgroundColorButton.Click += new System.EventHandler(this.BackgroundColorButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.NavajoWhite;
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearButton.Location = new System.Drawing.Point(521, 261);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(120, 49);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Очистить поле";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.startButton.Location = new System.Drawing.Point(521, 160);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(120, 32);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Старт";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.MediumPurple;
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.stopButton.Location = new System.Drawing.Point(521, 198);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(120, 32);
+            this.stopButton.TabIndex = 6;
+            this.stopButton.Text = "Стоп";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // logoLabel
+            // 
+            this.logoLabel.AutoSize = true;
+            this.logoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.logoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.logoLabel.ForeColor = System.Drawing.Color.Black;
+            this.logoLabel.Location = new System.Drawing.Point(525, 444);
+            this.logoLabel.Name = "logoLabel";
+            this.logoLabel.Size = new System.Drawing.Size(116, 24);
+            this.logoLabel.TabIndex = 7;
+            this.logoLabel.Text = "MartinBroni";
+            // 
+            // telegramButton
+            // 
+            this.telegramButton.BackColor = System.Drawing.Color.SkyBlue;
+            this.telegramButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.telegramButton.ForeColor = System.Drawing.Color.Transparent;
+            this.telegramButton.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.telegramButton.Location = new System.Drawing.Point(521, 471);
+            this.telegramButton.Name = "telegramButton";
+            this.telegramButton.Size = new System.Drawing.Size(120, 41);
+            this.telegramButton.TabIndex = 11;
+            this.telegramButton.Text = "Telegram";
+            this.telegramButton.UseVisualStyleBackColor = false;
+            this.telegramButton.Click += new System.EventHandler(this.TelegramButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(650, 526);
+            this.Controls.Add(this.telegramButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.backgroundColorButton);
+            this.Controls.Add(this.player2ColorButton);
+            this.Controls.Add(this.player1ColorButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.logoLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "MainForm";
+            this.Text = "Игра в жизнь";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Button player1ColorButton;
+        private System.Windows.Forms.Button player2ColorButton;
+        private System.Windows.Forms.Button backgroundColorButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label logoLabel;
+        private Button telegramButton;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
